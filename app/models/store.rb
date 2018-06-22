b1fe1c9b5552076d4ab098bb8b44cc932c7c7723
hotuta <hotuta@users.noreply.github.com>
2018-06-22 10:12:18 +0900

    デッドロック再対策

--- app/models/store.rb
+++ app/models/store.rb
@@ -122,36 +122,36 @@ class Store < ApplicationRecord
 
       ActiveSupport::Dependencies.interlock.permit_concurrent_loads do
         Parallel.each(bodies, in_processes: 3) do |body|
+          sleep 2
+
+          predictions_res = RestClient.get('https://www.ubereats.com/rtapi/locations/v2/predictions') {|predictions_response| predictions_response}
+          header = {x_csrf_token: predictions_res.headers[:x_csrf_token], content_type: 'application/json', Connection: 'keep-alive', cookies: predictions_res.cookies}
+          res = RestClient.post('https://www.ubereats.com/rtapi/eats/v1/bootstrap-eater', body.to_json, header) {|response| response}
+
+          json = res.body
+          hash = JSON.parse(json)
+          next if hash["marketplace"]["feed"].empty? rescue nil
+          parsed = hash["marketplace"]["feed"]["storesMap"] rescue nil
+          next if parsed.empty? rescue nil
+          next if parsed.nil?
+
+          stores = []
+          parsed.each.with_index(1) do |store|
+            stores_hash = store[1]
+            store = Store.new
+            store.area = area
+            store.url = "https://www.ubereats.com/stores/#{stores_hash["uuid"]}"
+            store.name = stores_hash["title"].gsub(/'/, "''")
+            store.coordinates = "#{stores_hash["location"]["latitude"]},#{stores_hash["location"]["longitude"]}"
+            store.latitude = stores_hash["location"]["latitude"]
+            store.longitude = stores_hash["location"]["longitude"]
+            store.registered_at = time
+            stores << store
+          end
+
+          columns = Store.column_names - ["id", "url", "created_at", "updated_at"]
           ActiveRecord::Base.connection_pool.with_connection do
             Rails.application.executor.wrap do
-              sleep 2
-
-              predictions_res = RestClient.get('https://www.ubereats.com/rtapi/locations/v2/predictions') {|predictions_response| predictions_response}
-              header = {x_csrf_token: predictions_res.headers[:x_csrf_token], content_type: 'application/json', Connection: 'keep-alive', cookies: predictions_res.cookies}
-              res = RestClient.post('https://www.ubereats.com/rtapi/eats/v1/bootstrap-eater', body.to_json, header) {|response| response}
-
-              json = res.body
-              hash = JSON.parse(json)
-              next if hash["marketplace"]["feed"].empty? rescue nil
-              parsed = hash["marketplace"]["feed"]["storesMap"] rescue nil
-              next if parsed.empty? rescue nil
-              next if parsed.nil?
-
-              stores = []
-              parsed.each.with_index(1) do |store|
-                stores_hash = store[1]
-                store = Store.new
-                store.area = area
-                store.url = "https://www.ubereats.com/stores/#{stores_hash["uuid"]}"
-                store.name = stores_hash["title"].gsub(/'/, "''")
-                store.coordinates = "#{stores_hash["location"]["latitude"]},#{stores_hash["location"]["longitude"]}"
-                store.latitude = stores_hash["location"]["latitude"]
-                store.longitude = stores_hash["location"]["longitude"]
-                store.registered_at = time
-                stores << store
-              end
-
-              columns = Store.column_names - ["id", "url", "created_at", "updated_at"]
               Store.import stores, recursive: true, on_duplicate_key_update: {conflict_target: [:url], columns: columns}
             end
           end
