f3f70a475963cc8df7a57e58192afafdf0542bdf
hotuta <hotuta@users.noreply.github.com>
2018-07-13 15:32:23 +0900

    横浜エリアの座標ファイルを追加

--- app/models/store.rb
+++ app/models/store.rb
@@ -51,6 +51,15 @@ class Store < ApplicationRecord
       end
     end
 
+    def dump_yokohama_coodinates
+      @prefecture = "神奈川県"
+      @target = "横浜"
+      get_coordinate
+      File.open("yokohama_coordinates.json", 'w') do |f|
+        JSON.dump(@coordinates, f)
+      end
+    end
+
     def dump_tokyo_coodinates
       @prefecture = "東京都"
       @target = ""
