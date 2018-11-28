cf842facd5e7d5a6d53adb1eb69c69909a0bd52e
hotuta <hotuta@users.noreply.github.com>
2018-11-28 16:02:20 +0900

    都道府県別1kmメッシュのリンクをコメントに追加

--- app/models/dump_coordinate.rb
+++ app/models/dump_coordinate.rb
@@ -3,6 +3,7 @@ class DumpCoordinate
   require 'csv'
 
   class << self
+    # 都道府県別1kmメッシュ https://www.geospatial.jp/ckan/dataset/npli-pref-1km
     def remove_duplication_output
       CSV.open('愛知県1km毎_重複削除.csv', 'w') do |csv|
         CSV.read('愛知県1km毎.csv').uniq.each { |r| csv << r }
