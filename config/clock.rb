ef6b3213d8f527646c4933d1fa565a3b04b9a486
hotuta <hotuta@users.noreply.github.com>
2019-02-06 20:44:28 +0900

    20時間毎に更新する設定をコメントアウト

--- config/clock.rb
+++ config/clock.rb
@@ -8,17 +8,17 @@ module Clockwork
   end
 
   # TODO: コメントアウトしたい。暫定として、20時間毎に定期実行することにして緊急対処。
-  every(20.hours, 'Store') do
-    Store.get_kawasaki_data
-    Store.edit_tokyo_mymaps
-    Store.edit_osaka_mymaps
-    Store.edit_yokohama_mymaps
-    Store.edit_kyoto_mymaps
-    Store.edit_kobe_mymaps
-    Store.edit_saitama_mymaps
-    Store.edit_nagoya_mymaps
-    Store.edit_fukuoka_mymaps
-  end
+  # every(20.hours, 'Store') do
+  #   Store.get_kawasaki_data
+  #   Store.edit_tokyo_mymaps
+  #   Store.edit_osaka_mymaps
+  #   Store.edit_yokohama_mymaps
+  #   Store.edit_kyoto_mymaps
+  #   Store.edit_kobe_mymaps
+  #   Store.edit_saitama_mymaps
+  #   Store.edit_nagoya_mymaps
+  #   Store.edit_fukuoka_mymaps
+  # end
 
   every(1.day, 'Store', at: ['4:00']) do
     # Store.edit_kawasaki_mymaps
