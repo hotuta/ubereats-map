a3c1083946488914491f4bf1d610388860dc8681
hotuta <hotuta@users.noreply.github.com>
2019-02-07 20:34:21 +0900

    rakeで定期処理の実行を行うため、誤爆防止でコメントアウト

--- config/clock.rb
+++ config/clock.rb
@@ -20,15 +20,15 @@ module Clockwork
   #   Store.edit_fukuoka_mymaps
   # end
 
-  every(1.day, 'Store', at: ['4:00']) do
-    # Store.edit_kawasaki_mymaps
-    Store.edit_tokyo_mymaps
-    Store.edit_osaka_mymaps
-    Store.edit_yokohama_mymaps
-    Store.edit_kyoto_mymaps
-    Store.edit_kobe_mymaps
-    Store.edit_saitama_mymaps
-    Store.edit_nagoya_mymaps
-    Store.edit_fukuoka_mymaps
-  end
+  # every(1.day, 'Store', at: ['4:00']) do
+  #   # Store.edit_kawasaki_mymaps
+  #   Store.edit_tokyo_mymaps
+  #   Store.edit_osaka_mymaps
+  #   Store.edit_yokohama_mymaps
+  #   Store.edit_kyoto_mymaps
+  #   Store.edit_kobe_mymaps
+  #   Store.edit_saitama_mymaps
+  #   Store.edit_nagoya_mymaps
+  #   Store.edit_fukuoka_mymaps
+  # end
 end
